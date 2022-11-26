c39d1d910b99b6d2abc8d9626497dc1a281635e9
SukritGaneshUIUC <sukritg2@illinois.edu>
2022-11-25 21:11:22 -0600

    some more playing around

---
+++ ast_example_0.py
@@ -0,0 +1,8 @@
+import ast
+import pprint
+
+def main():
+    print("hello there")
+
+if __name__ == "__main__":
+    main()
---
+++ sample_program_0.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def add_two(x):
+    return x + 2
+
+num1 = 10
+num2 = add_two(num1)
+
+# hello this is a comment
+
+
+
+
+
+print("Number 2:", num2)
