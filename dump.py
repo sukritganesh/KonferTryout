42b7a6508218346586827161fa266f64648d4464
SukritGaneshUIUC <sukritg2@illinois.edu>
2022-11-26 18:10:03 -0600

    successfully added one print statement; next step will be printing all arguments of function

--- dump.py
+++ dump.py
@@ -1,3 +1,17 @@
-stuff = 1+4
-thing = 3+4
-print 'Beautiful result:', stuff 
+def isEnoughData(self,df,columns):
+        '''
+        Args:
+            df -> input dataframe
+            columns -> target feature name(list)
+        Returns:
+            It return True/False based on distinct value count of the target feature
+        '''
+        print('your move')
+        for col in columns:
+            distcnt = np.count_nonzero(df[col], axis=0)
+            if (distcnt < 10):
+                return False
+            # sd = df[col].std()
+        return True
+
+    
